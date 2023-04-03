2110ba9d28c450ef9c0f65fec09e24f2e9b318ce
singt551 <112576297+singt551@users.noreply.github.com>
2023-04-03 15:05:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+// function test1(){
+//     var a = document.getElementById("heading")
+//     a.style.color= "blue"
+//     }
+    
+//     function test2(){
+//         var a = document.getElementById("heading")
+//         a.style.color= "yellow"
+//     }
+    
+//     function test3(){
+//         var a = document.getElementsByClassName("page")
+//         a[0].style.backgroundColor = "black"
+//     }
+    
+    
+    function choice1(x){
+        var a = document.getElementsByClassName("red")
+        if (x=="a1"){
+            a[0].style.backgroundColor = "red"
+        }
+        else if(x=="b1"){
+            a[1].style.backgroundColor = "red"
+        }
+        else if(x=="c1"){
+            a[2].style.backgroundColor = "red"
+        }
+        else if(x=="a2"){
+            a[3].style.backgroundColor = "red"
+        }
+        else if(x=="b2"){
+            a[4].style.backgroundColor = "red"
+        }
+        else if(x=="c2"){
+            a[5].style.backgroundColor = "red"
+        }
+        else if(x=="a3"){
+            a[6].style.backgroundColor = "red"
+        }
+        else if(x=="b3"){
+            a[7].style.backgroundColor = "red"
+        }
+        else if(x=="c3"){
+            a[8].style.backgroundColor = "red"
+        }
+        else if(x=="a4"){
+            a[9].style.backgroundColor = "red"
+        }
+        else if(x=="b4"){
+            a[10].style.backgroundColor = "red"
+        }
+        else if(x=="c4"){
+            a[11].style.backgroundColor = "red"
+        }
+        else if(x=="a5"){
+            a[12].style.backgroundColor = "red"
+        }
+        else if(x=="b5"){
+            a[13].style.backgroundColor = "red"
+        }
+        else if(x=="c5"){
+            a[14].style.backgroundColor = "red"
+        }
+    }
+    
+    function choice2(x){
+        var a = document.getElementsByClassName("green")
+        if (x==1){
+            a[0].style.backgroundColor = "green"
+        }
+        else if (x==2){
+            a[1].style.backgroundColor = "green"
+        }
+        else if (x==3){
+            a[2].style.backgroundColor = "green"
+        }
+        else if (x==4){
+            a[3].style.backgroundColor = "green"
+        }
+        else if (x==5){
+            a[4].style.backgroundColor = "green"
+        }
+    }
+
+    
+    // function hover(){
+    //     var a = document.getElementById("heading")
+    //     a.style.color= "pink"
+    //     a.style.fontSize= "50px"
+    // }
+    
+    // function hoverOut(){
+    //     var a = document.getElementById("heading")
+    //     a.style.color= "black"
+    //     a.style.fontSize= "20px"
+    // }
